c2f1cd8dd53ab35aff7bc23948d5f0de8c6371b0
Marcos Dourado <marcos.filho18@gmail.com>
2019-06-08 02:34:15 -0300

    Nova alteracao

--- Projeto/src/view/usuario/TelaConsultaPercurso.java
+++ Projeto/src/view/usuario/TelaConsultaPercurso.java
@@ -6,6 +6,8 @@ import model.Cidade;
 import mode.Instituicao;
 
 public class TelaConsultaPercurso extends Tela{
+	public String telaIni = "\nDigite 1 para listar percursos ou 2 para pesquisar percuros: ";
+	public String telaErro = "\nNao foi encontrado nenhum ponto com essas informacoes. \n\nDigite 1 para listar percursos ou 2 para pesquisar percurso: ";
 	public String telaPercurso;
 
 	public void setTelaPercurso(ArrayList<Rota> rotas, Instituicao instituicao){
