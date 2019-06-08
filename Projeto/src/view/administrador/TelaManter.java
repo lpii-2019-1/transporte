21608fc6f50e1a393c50433f9122959b0d76d4df
Marcos Dourado <marcos.filho18@gmail.com>
2019-06-08 05:40:58 -0300

    Reformulado os pacotes

--- Projeto/src/view/administrador/TelaManter.java
+++
@@ -1,7 +0,0 @@
-package administrador;
-
-import view.Tela;
-
-public class TelaCidade extends Tela{
-	public String tela = "\n1 - Consutar \n2 - Cadastrar \n3 - Editar \n4 - Excluir \n0 - Sair";
-}
--- Projeto/src/view/usuario/TelaConsultaTurno.java
+++
@@ -1,7 +0,0 @@
-package usuario;
-
-import view.Tela;
-
-public class TelaConsultaTurno extends Tela{
-	public String telaIni = "\nTurnos: \n    1 - Matutino \n    2 - Vespertino \n    3 - Noturno \n    0 - Sair\n\nDigite o numero correspondente: ";
-}
