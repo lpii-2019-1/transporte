80b4bb85a4383859dbc8c2129d6e29ba9aef9b67
Marcos Dourado <marcos.filho18@gmail.com>
2019-06-08 05:48:00 -0300

    Update

---
+++ Projeto/src/viewAdministrador/TelaManter.java
@@ -0,0 +1,7 @@
+package viewAdministrador;
+
+import view.Tela;
+
+public class TelaCidade extends Tela{
+	public String tela = "\n1 - Consutar \n2 - Cadastrar \n3 - Editar \n4 - Excluir \n0 - Sair";
+}
---
+++ Projeto/src/viewUsuario/TelaConsultaCidade.java
@@ -0,0 +1,8 @@
+package viewUsuario;
+
+import view.Tela;
+
+public class TelaConsultaCidade extends Tela{
+	public String telaIni = "\nDigite o nome de uma cidade (Ex.: Ceres GO): ";
+	public String telaErro = "\nCidade não encontrada. \n\nDigite o nome de uma cidade (Ex.: Ceres GO): ";
+}
