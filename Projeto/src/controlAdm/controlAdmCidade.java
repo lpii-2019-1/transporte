499c8a21f580e0ff5b58e51410654f7cd9f7a1ed
Marcos Winicios <pmarcoswinicios@gmail.com>
2019-06-10 07:19:49 -0300

    Concluída Classe ControlerAdmCidade

--- Projeto/src/controlAdm/controlAdmCidade.java
+++ Projeto/src/controlAdm/controlAdmCidade.java
@@ -1,7 +1,7 @@
 package controlAdm;
 
 import model.Cidade;
-
+import model.Instituicao;
 
 import java.util.ArrayList;
 
@@ -10,19 +10,78 @@ import dao.CidadeDAO;
 public class controlAdmCidade {
 	private Cidade cidadeSelecionada;
 	
+	private ArrayList<Cidade> cidades;
+	private ArrayList<Cidade> todasCidades;
+	private ArrayList<Instituicao> instituicoes;
 	
-	public void selecionarCidade(ArrayList<String> valores) { // nome, uf
+	
+	
+	/*Metodos de Consulta*/
+	public Cidade consultarCidadeNomeUf(ArrayList<String> valores) {
+		this.cidadeSelecionada = new Cidade(valores.get(0), valores.get(2));
 		CidadeDAO cDAO = new CidadeDAO();
-		this.cidadeSelecionada =  new Cidade(valores.get(0), valores.get(1));
-		this.cidadeSelecionada = cDAO.consultarNomeUf(this.cidadeSelecionada, 2 );
+		this.cidadeSelecionada = cDAO.consultarNomeUf(this.cidadeSelecionada, 2);
+		return this.cidadeSelecionada;
 	}
 	
-	public void editarNomeCidade(String nome) {
+	public ArrayList<Cidade> consultarNome(String nome){
+		CidadeDAO cDAO = new CidadeDAO();
+		this.cidadeSelecionada = new Cidade();
+		this.cidadeSelecionada.setNome(nome);
+		this.cidades = cDAO.consultarNome(this.cidadeSelecionada, 2);
+		return this.cidades;
+	}
+	
+	public ArrayList<Cidade> consultarUf(String uf){
+		CidadeDAO cDAO = new CidadeDAO();
+		this.cidadeSelecionada = new Cidade();
+		this.cidadeSelecionada.setUf(uf);
+		this.cidades = cDAO.consultarUf(this.cidadeSelecionada, 2);
+		return this.cidades;
+	}
+	
+	/*Metodos de Edição*/
+	
+	public boolean editarNomeCidade(String nome) {
 		CidadeDAO cDAO = new CidadeDAO();
 		this.cidadeSelecionada.setNome(nome);
-		cDAO.editarNome(this.cidadeSelecionada);
+		if(cDAO.editarNome(this.cidadeSelecionada)) {
+			return true;
+		}else {
+			return false;
+		}
 	}
 
+	public boolean editarUfCidade(String uf) {
+		CidadeDAO cDAO = new CidadeDAO();
+		this.cidadeSelecionada.setUf(uf);
+		if(cDAO.editarUf(this.cidadeSelecionada)) {;
+			return true;
+		}else {
+			return false;
+		}
+	}
+	public boolean editarValidacaoCidade(String validacao) { // Tem possibilidade de Mudae o parametro para int.Depende da confirmação do M.D(Comunista)
+		CidadeDAO cDAO = new CidadeDAO();
+		if(validacao == "disponivel") {
+			this.cidadeSelecionada.setValidacao(1);
+		}
+		if(cDAO.editarValidacao(this.cidadeSelecionada)) {;
+			return true;
+		}else {
+			return false;
+		}
+	}
+	
+	/*Metodo para listar todas cidades do banco*/
+	
+	public ArrayList<Cidade> listarTodasCidades() {
+		CidadeDAO cDAO = new CidadeDAO();
+		this.todasCidades = cDAO.listarCidades(2);
+		return this.todasCidades;
+	}
+	
+	/*Metodo de iniserir*/
 	public boolean inserirCidade(ArrayList<String> valores) { //nome, uf e talvez Disponibilidade
 		String nome  = valores.get(0);
 		String uf = valores.get(1);
@@ -31,8 +90,7 @@ public class controlAdmCidade {
 		Cidade c = new Cidade(nome, uf);
 		c = cDAO.consultarNomeUf(c, 2);
 		if(c.getId() ==0) {
-			cDAO.inserirCidade(c);
-					
+			cDAO.inserirCidade(c);					
 			if(valores.size() == 3) {
 				if(valores.get(2) == "Disponivel") {
 					c.setValidacao(1);
@@ -46,6 +104,22 @@ public class controlAdmCidade {
 			return false;
 		}
 	}
+	/*Metodo para excluir cidade*/
+	
+	public boolean excluirCidade() {
+		CidadeDAO cDAO = new CidadeDAO();
+		if(cDAO.excluirCidade(this.cidadeSelecionada)) {
+			return true;
+		}else {
+			return false;
+		}
+	}
+	/*Listar Todas as instituicoes da cidade*/
+	public ArrayList<Instituicao> listarInstituicoesCidade(){
+		CidadeDAO cDAO = new CidadeDAO();
+		this.instituicoes = cDAO.listarInstituicoes(this.cidadeSelecionada, 2);
+		return this.instituicoes;
+	}
 	
 	
 	//Getters and Setters
@@ -57,5 +131,34 @@ public class controlAdmCidade {
 
 	public void setCidadeSelecionada(Cidade cidadeSelecionada) {
 		this.cidadeSelecionada = cidadeSelecionada;
+	}
+	
+	public ArrayList<Cidade> getCidades() {
+		return cidades;
+	}
+
+	public void setCidades(ArrayList<Cidade> cidades) {
+		this.cidades = cidades;
+	}
+
+	public ArrayList<Cidade> getTodasCidades() {
+		return todasCidades;
+	}
+
+	public void setTodasCidades(ArrayList<Cidade> todasCidades) {
+		this.todasCidades = todasCidades;
+	}
+
+	public ArrayList<Instituicao> getInstituicoes() {
+		return instituicoes;
+	}
+
+	public void setInstituicoes(ArrayList<Instituicao> instituicoes) {
+		this.instituicoes = instituicoes;
+	}
+
+	public void selecionarCidade(int i) { 
+		i--;
+		this.cidadeSelecionada = cidades.get(i);
 	}
 }
