c168acfa727db17d0d53a24883ccdebb3d132c52
Marcos Lima <marcosanthony21@hotmail.com>
2019-06-12 08:42:16 -0300

    Alterei metodo de inserirPonto no Controlador de Ponto,
    Alteração no metodo de inserirPonto na class ControlAdmPonto, falta fazer o  metodo de ConsultarIdRota

--- Projeto/src/controlAdm/ControlAdmPonto.java
+++ Projeto/src/controlAdm/ControlAdmPonto.java
@@ -1,42 +1,30 @@
 package controlAdm;
 
 import java.util.ArrayList;
-
 import dao.PontoDAO;
-import model.Cidade;
 import model.Ponto;
 import model.Rota;
 
 public class ControlAdmPonto {
 
 	private Ponto pontoSelecionado;
-	private Rota rotaSelecionada;
 
-	private ArrayList<ArrayList<Ponto>> pontosTodasRotas;
 	private ArrayList<Ponto> pontos;
-	
 
-	public boolean verificaId(Cidade c) {
-		if (c.getId() == 0) {
-			return false;
-		} else {
+	public boolean inserirPonto(String endereco) {
+		PontoDAO poDAO = new PontoDAO();
+		this.pontoSelecionado = new Ponto(endereco);
+		if (poDAO.inserirPonto(this.pontoSelecionado)) {
 			return true;
+		} else {
+			return false;
 		}
 	}
-	
-	/** Refazer **/
-	public void inserirPonto(String endereco) {
-		PontoDAO poDAO = new PontoDAO();
-		Ponto po = new Ponto();
-		po.setEndereco(endereco);
-		this.setPontoSelecionado(po);
-		poDAO.inserirPonto(this.getPontoSelecionado());
-	}
-		
+
 	public boolean editarEndereco(String endereco) {
 		PontoDAO poDAO = new PontoDAO();
 		this.pontoSelecionado.setEndereco(endereco);
-		if(poDAO.editarEndereco(this.pontoSelecionado)) {
+		if (poDAO.editarEndereco(this.pontoSelecionado)) {
 			return true;
 		} else {
 			return false;
@@ -45,10 +33,10 @@ public class ControlAdmPonto {
 
 	public boolean editarValidacao(String validacao) {
 		PontoDAO poDAO = new PontoDAO();
-		if(validacao.equals(validacao)) {
+		if (validacao.equals(validacao)) {
 			this.pontoSelecionado.setValidacao(1);
 		}
-		if(poDAO.editarValidacao(this.pontoSelecionado)) {
+		if (poDAO.editarValidacao(this.pontoSelecionado)) {
 			return true;
 		} else {
 			return false;
@@ -68,12 +56,14 @@ public class ControlAdmPonto {
 		this.pontoSelecionado = poDAO.consultarId(id, 2);
 		return this.pontos;
 	}
-	
-	/** Verificar erro metodo **//*
-	public Ponto consultarIdRota(Rota rota) {
+
+	/** Verificar erro metodo **/
+	/*
+		public ArrayList<Ponto> consultarIdRota(Rota rota) {
 		PontoDAO poDAO = new PontoDAO();
-		this.setPontoSelecionado(poDAO.consultarIdRota(rota, 2));
-		return this.getPontoSelecionado();
+		this.pontoSelecionado = new Ponto();
+		this.pontos = poDAO.consultarIdRota(rota, 2);
+		return this.pontos;
 	}
 	*/
 	
@@ -83,10 +73,10 @@ public class ControlAdmPonto {
 		this.pontos = poDAO.listarPontos(2);
 		return this.pontos;
 	}
-	
+
 	public boolean excluirPonto(int x) {
 		PontoDAO poDAO = new PontoDAO();
-		if(poDAO.excluirPonto(this.pontoSelecionado)) {
+		if (poDAO.excluirPonto(this.pontoSelecionado)) {
 			return true;
 		} else {
 			return false;
@@ -100,13 +90,13 @@ public class ControlAdmPonto {
 	public void setPontoSelecionado(Ponto pontoSelecionado) {
 		this.pontoSelecionado = pontoSelecionado;
 	}
-
-	public Rota getRotaSelecionada() {
-		return rotaSelecionada;
+	
+	public ArrayList<Ponto> getPontos() {
+		return pontos;
 	}
 
-	public void setRotaSelecionada(Rota rotaSelecionada) {
-		this.rotaSelecionada = rotaSelecionada;
+	public void setPontos(ArrayList<Ponto> pontos) {
+		this.pontos = pontos;
 	}
 
 }
