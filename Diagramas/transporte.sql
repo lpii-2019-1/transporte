4135ffb200149e25d0091658439e3fa096e15187
Marcos Dourado <marcos.filho18@gmail.com>
2019-06-13 04:52:03 -0300

    Atualizei o banco, tá com a trigger, todas as funcionalidades necessarias + dados

--- Diagramas/transporte.sql
+++ Diagramas/transporte.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.3
+-- version 4.9.0.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1:3306
--- Generation Time: 11-Jun-2019 às 20:48
--- Versão do servidor: 5.7.23
--- versão do PHP: 7.2.10
+-- Host: 127.0.0.1
+-- Tempo de geração: 13-Jun-2019 às 09:47
+-- Versão do servidor: 10.3.15-MariaDB
+-- versão do PHP: 7.3.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -19,24 +19,21 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `transporte`
+-- Banco de dados: `transporte`
 --
-	CREATE DATABASE projeto_transporte;
-    USE projeto_transporte;
+
 -- --------------------------------------------------------
 
 --
 -- Estrutura da tabela `cidade`
 --
 
-DROP TABLE IF EXISTS `cidade`;
-CREATE TABLE IF NOT EXISTS `cidade` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `cidade` (
+  `id` int(11) NOT NULL,
   `nome` varchar(45) NOT NULL,
   `uf` char(2) NOT NULL,
-  `validacao` int(11) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+  `validacao` int(11) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Extraindo dados da tabela `cidade`
@@ -44,7 +41,8 @@ CREATE TABLE IF NOT EXISTS `cidade` (
 
 INSERT INTO `cidade` (`id`, `nome`, `uf`, `validacao`) VALUES
 (1, 'Ceres', 'GO', 1),
-(2, 'Rialma', 'GO', 0);
+(3, 'Rialma', 'GO', 0),
+(4, 'Santa Isabel', 'GO', 0);
 
 -- --------------------------------------------------------
 
@@ -52,18 +50,14 @@ INSERT INTO `cidade` (`id`, `nome`, `uf`, `validacao`) VALUES
 -- Estrutura da tabela `horario`
 --
 
-DROP TABLE IF EXISTS `horario`;
-CREATE TABLE IF NOT EXISTS `horario` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `horario` (
+  `id` int(11) NOT NULL,
   `horario_saida` time NOT NULL,
   `horario_regresso` time NOT NULL,
   `id_turno` int(11) NOT NULL,
-  `validacao` int(11) NOT NULL,
-  `id_rota` int(11) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `fk_horario_turno1_idx` (`id_turno`),
-  KEY `rota_idx` (`id_rota`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+  `validacao` int(11) NOT NULL DEFAULT 0,
+  `id_rota` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Extraindo dados da tabela `horario`
@@ -79,17 +73,14 @@ INSERT INTO `horario` (`id`, `horario_saida`, `horario_regresso`, `id_turno`, `v
 -- Estrutura da tabela `instituicao`
 --
 
-DROP TABLE IF EXISTS `instituicao`;
-CREATE TABLE IF NOT EXISTS `instituicao` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `instituicao` (
+  `id` int(11) NOT NULL,
   `nome` varchar(45) NOT NULL,
   `endereco` varchar(100) NOT NULL,
   `telefone` varchar(13) NOT NULL,
   `id_cidade` int(11) NOT NULL,
-  `validacao` int(11) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `fk_instituicao_cidade1_idx` (`id_cidade`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+  `validacao` int(11) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Extraindo dados da tabela `instituicao`
@@ -97,8 +88,8 @@ CREATE TABLE IF NOT EXISTS `instituicao` (
 
 INSERT INTO `instituicao` (`id`, `nome`, `endereco`, `telefone`, `id_cidade`, `validacao`) VALUES
 (1, 'IF Goiano', 'Meio do Mato', '3307-1111', 1, 1),
-(2, 'UEG', 'Rua qualquer', '3307-1212', 1, 0),
-(3, 'Facer', 'Av. Brasil', '3307-1313', 1, 1);
+(3, 'Facer', 'Av. Brasil', '3307-1313', 1, 1),
+(4, 'UniEvangelica', 'Lugar Nenhum', '1222-3331', 1, 0);
 
 -- --------------------------------------------------------
 
@@ -106,17 +97,15 @@ INSERT INTO `instituicao` (`id`, `nome`, `endereco`, `telefone`, `id_cidade`, `v
 -- Estrutura da tabela `onibus`
 --
 
-DROP TABLE IF EXISTS `onibus`;
-CREATE TABLE IF NOT EXISTS `onibus` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `onibus` (
+  `id` int(11) NOT NULL,
   `placa` varchar(45) NOT NULL,
   `cor` varchar(45) NOT NULL,
   `motorista` varchar(45) NOT NULL,
   `mensalidade` decimal(4,2) NOT NULL,
-  `validacao` int(11) NOT NULL,
-  `telefone` varchar(45) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+  `validacao` int(11) NOT NULL DEFAULT 0,
+  `telefone` varchar(45) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Extraindo dados da tabela `onibus`
@@ -132,13 +121,11 @@ INSERT INTO `onibus` (`id`, `placa`, `cor`, `motorista`, `mensalidade`, `validac
 -- Estrutura da tabela `ponto`
 --
 
-DROP TABLE IF EXISTS `ponto`;
-CREATE TABLE IF NOT EXISTS `ponto` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `ponto` (
+  `id` int(11) NOT NULL,
   `endereco` varchar(45) NOT NULL,
-  `validacao` int(11) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+  `validacao` int(11) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Extraindo dados da tabela `ponto`
@@ -148,8 +135,7 @@ INSERT INTO `ponto` (`id`, `endereco`, `validacao`) VALUES
 (1, 'ponto1', 1),
 (2, 'ponto2', 1),
 (3, 'ponto3', 1),
-(4, 'ponto4', 0),
-(5, 'ponto5', 1);
+(4, 'ponto4', 1);
 
 -- --------------------------------------------------------
 
@@ -157,38 +143,30 @@ INSERT INTO `ponto` (`id`, `endereco`, `validacao`) VALUES
 -- Estrutura da tabela `rota`
 --
 
-DROP TABLE IF EXISTS `rota`;
-CREATE TABLE IF NOT EXISTS `rota` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `rota` (
+  `id` int(11) NOT NULL,
   `inicio` varchar(45) NOT NULL,
   `fim` varchar(45) NOT NULL,
-  `identificador` int(11) NOT NULL DEFAULT '0',
   `id_onibus` int(11) NOT NULL,
-  `validacao` int(11) NOT NULL,
-  `percurso` varchar(1000) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `idOnibus_idx` (`id_onibus`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
+  `identificador` int(11) NOT NULL DEFAULT 0,
+  `validacao` int(11) NOT NULL DEFAULT 0,
+  `percurso` varchar(1000) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Extraindo dados da tabela `rota`
 --
 
-INSERT INTO `rota` (`id`, `inicio`, `fim`, `identificador`, `id_onibus`, `validacao`, `percurso`) VALUES
+INSERT INTO `rota` (`id`, `inicio`, `fim`, `id_onibus`, `identificador`, `validacao`, `percurso`) VALUES
 (1, 'Santa Isabel', 'IF Goiano', 1, 1, 1, 'De la pra ca'),
-(2, 'Itapaci', 'UEG', 1, 2, 1, 'De ca pra la'),
-(3, 'Rialma', 'IF Goiano', 2, 1, 1, 'Setor A, Setor B, Setor C'),
-(5, 'Santa Terezinha', 'IF Goiano', 2, 2, 1, 'Santa Terezinha, Itapaci, Rialma, IF Goiano'),
-(6, 'Rubiataba', 'IF Goiano', 3, 2, 1, 'Rubiataba, Rialma, UEG(Ceres), IF Goiano'),
-(8, 'Rialma', 'IF Goiano', 3, 1, 1, 'Setor 1, Setor 2, Setor 3, IF Goiano');
+(2, 'Itapaci', 'UEG', 2, 1, 1, 'De ca pra la'),
+(3, 'Rialma', 'Facer', 2, 2, 2, 'Daqui pra li');
 
 --
 -- Acionadores `rota`
 --
-DROP TRIGGER IF EXISTS `identifica_rota`;
 DELIMITER $$
-CREATE TRIGGER `identifica_rota` BEFORE INSERT ON `rota` FOR EACH ROW 
-BEGIN
+CREATE TRIGGER `identifica_rota` BEFORE INSERT ON `rota` FOR EACH ROW BEGIN
 	SET NEW.identificador  = (
 		SELECT COUNT(*) FROM rota 
 		WHERE id_onibus = NEW.id_onibus
@@ -203,15 +181,11 @@ DELIMITER ;
 -- Estrutura da tabela `rota_has_instituicao`
 --
 
-DROP TABLE IF EXISTS `rota_has_instituicao`;
-CREATE TABLE IF NOT EXISTS `rota_has_instituicao` (
-  `Id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `rota_has_instituicao` (
+  `Id` int(11) NOT NULL,
   `id_rota` int(11) NOT NULL,
-  `id_instituicao` int(11) NOT NULL,
-  PRIMARY KEY (`Id`),
-  KEY `fk_rota_has_instituicao_instituicao1_idx` (`id_instituicao`),
-  KEY `fk_rota_has_instituicao_rota1_idx` (`id_rota`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+  `id_instituicao` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Extraindo dados da tabela `rota_has_instituicao`
@@ -219,9 +193,7 @@ CREATE TABLE IF NOT EXISTS `rota_has_instituicao` (
 
 INSERT INTO `rota_has_instituicao` (`Id`, `id_rota`, `id_instituicao`) VALUES
 (1, 2, 1),
-(2, 2, 2),
 (3, 1, 3),
-(4, 1, 2),
 (5, 1, 1);
 
 -- --------------------------------------------------------
@@ -230,16 +202,12 @@ INSERT INTO `rota_has_instituicao` (`Id`, `id_rota`, `id_instituicao`) VALUES
 -- Estrutura da tabela `rota_has_ponto`
 --
 
-DROP TABLE IF EXISTS `rota_has_ponto`;
-CREATE TABLE IF NOT EXISTS `rota_has_ponto` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `rota_has_ponto` (
+  `id` int(11) NOT NULL,
   `id_rota` int(11) NOT NULL,
   `id_ponto` int(11) NOT NULL,
-  `ordem` int(11) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `fk_rota_has_ponto_ponto1_idx` (`id_ponto`),
-  KEY `fk_rota_has_ponto_rota1_idx` (`id_rota`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+  `ordem` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Extraindo dados da tabela `rota_has_ponto`
@@ -249,7 +217,6 @@ INSERT INTO `rota_has_ponto` (`id`, `id_rota`, `id_ponto`, `ordem`) VALUES
 (1, 1, 1, 1),
 (2, 1, 3, 2),
 (3, 1, 4, 3),
-(4, 2, 5, 1),
 (5, 2, 1, 2),
 (6, 2, 3, 3);
 
@@ -259,12 +226,10 @@ INSERT INTO `rota_has_ponto` (`id`, `id_rota`, `id_ponto`, `ordem`) VALUES
 -- Estrutura da tabela `turno`
 --
 
-DROP TABLE IF EXISTS `turno`;
-CREATE TABLE IF NOT EXISTS `turno` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `turno` varchar(11) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+CREATE TABLE `turno` (
+  `id` int(11) NOT NULL,
+  `turno` varchar(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Extraindo dados da tabela `turno`
@@ -276,7 +241,131 @@ INSERT INTO `turno` (`id`, `turno`) VALUES
 (3, 'Noturno');
 
 --
--- Constraints for dumped tables
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices para tabela `cidade`
+--
+ALTER TABLE `cidade`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices para tabela `horario`
+--
+ALTER TABLE `horario`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_horario_turno1_idx` (`id_turno`),
+  ADD KEY `rota_idx` (`id_rota`);
+
+--
+-- Índices para tabela `instituicao`
+--
+ALTER TABLE `instituicao`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_instituicao_cidade1_idx` (`id_cidade`);
+
+--
+-- Índices para tabela `onibus`
+--
+ALTER TABLE `onibus`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices para tabela `ponto`
+--
+ALTER TABLE `ponto`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices para tabela `rota`
+--
+ALTER TABLE `rota`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `idOnibus_idx` (`id_onibus`);
+
+--
+-- Índices para tabela `rota_has_instituicao`
+--
+ALTER TABLE `rota_has_instituicao`
+  ADD PRIMARY KEY (`Id`),
+  ADD KEY `fk_rota_has_instituicao_instituicao1_idx` (`id_instituicao`),
+  ADD KEY `fk_rota_has_instituicao_rota1_idx` (`id_rota`);
+
+--
+-- Índices para tabela `rota_has_ponto`
+--
+ALTER TABLE `rota_has_ponto`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_rota_has_ponto_ponto1_idx` (`id_ponto`),
+  ADD KEY `fk_rota_has_ponto_rota1_idx` (`id_rota`);
+
+--
+-- Índices para tabela `turno`
+--
+ALTER TABLE `turno`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `cidade`
+--
+ALTER TABLE `cidade`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT de tabela `horario`
+--
+ALTER TABLE `horario`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de tabela `instituicao`
+--
+ALTER TABLE `instituicao`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT de tabela `onibus`
+--
+ALTER TABLE `onibus`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de tabela `ponto`
+--
+ALTER TABLE `ponto`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT de tabela `rota`
+--
+ALTER TABLE `rota`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de tabela `rota_has_instituicao`
+--
+ALTER TABLE `rota_has_instituicao`
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT de tabela `rota_has_ponto`
+--
+ALTER TABLE `rota_has_ponto`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT de tabela `turno`
+--
+ALTER TABLE `turno`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- Restrições para despejos de tabelas
 --
 
 --
